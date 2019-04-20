a17f46a48f80b7ba922b5a87c8673f10cbe5b3aa
Patrick Bogen <pdbogen@cernu.us>
2019-04-20 11:20:21 -0700

    fix typod scheme

--- oauth2.go
+++ oauth2.go
@@ -7,5 +7,5 @@ import (
 // Endpoint is Withing's OAuth 2.0 endpoint.
 var Oauth2Endpoint = oauth2.Endpoint{
 	AuthURL:  "https://account.withings.com/oauth2_user/authorize2",
-	TokenURL: "hhttps://account.withings.com/oauth2/token",
+	TokenURL: "https://account.withings.com/oauth2/token",
 }
